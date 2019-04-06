449bd9630d317bdb28f22ae28e1c7f8e6a28d052
JHeck0203 <47832260+JHeck0203@users.noreply.github.com>
2019-04-06 12:38:19 -0400

    Update runanalysis.r

--- runanalysis.r
+++ runanalysis.r
@@ -39,3 +39,6 @@ colnames(Sub_total) <- "subject"
 total <- cbind(X_total, activitylabel, Sub_total)
 total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
 write.table(total_mean, file = "./UCI HAR Dataset/tidy_data.txt", row.names = FALSE, col.names = TRUE)
+
+
+
